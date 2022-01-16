b42e5e2291825c7f4283713cab9f94f678c8c75c
avinash_evaluation <avinash.kumar550@gmail.com>
2022-01-16 14:48:04 +0530

    electronics.html

---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+function navbar()
+{
+
+
+    return `  <div id="navbar">
+    <h3><a href="/">Home</a></h3>
+    <div id="option">
+        <h3><a href="/electronics.html">Electronics</a> </h3>
+                 <h3><a href="/jewelery.html">Jewellary</a> </h3>
+                 <h3><a href="">Sign In</a>  </h3>
+                 <h3><a href="">Sign Up</a></h3>
+    </div>
+
+</div>`
+}
+
+export default navbar
